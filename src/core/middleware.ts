4367afdc6e9aa02f4422fc56c329f70245d5e2fe
Charles <charleshh1995@gmail.com>
2018-10-19 16:54:10 -0500

    Update package scripts for prod build

--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 export async function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization;
   try {
-    const decodedToken = await verifyJWToken(token);
+    const decodedToken: any = await verifyJWToken(token);
     req.user = decodedToken.data;
   } catch (err) {
     res.status(403).json({ message: 'Invalid authorization' });
