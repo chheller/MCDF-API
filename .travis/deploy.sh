01aa647e1ee8d54424b3e39820436576add4f5d4
Charles <charleshh1995@gmail.com>
2018-12-22 14:23:15 -0600

    Fix deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,10 +2,10 @@
 set -e
 set -u
 HOST_IP=$1
-openssl aes-256-cbc -K $encrypted_8e5d82c714da_key -iv $encrypted_8e5d82c714da_iv -in .travis/key.enc -out .travis/key -d
+openssl aes-256-cbc -K $encrypted_71a8e2a8b8b0_key -iv $encrypted_71a8e2a8b8b0_iv -in .travis/key.enc -out .travis/key -d
 eval "$(ssh-agent -s)"
 ssh-keyscan -H $DOCKER_SWARM_HOST_IP >> $HOME/.ssh/known_hosts
 chmod 600 .travis/key
 ssh-add .travis/key
 
-echo $DOCKER_SWARM_USER_PASSWORD | ssh swarm@$HOST_IP sudo -S docker service update --image chheller/stator stator-api
+echo $DOCKER_SWARM_USER_PASSWORD | ssh swarm@$DOCKER_SWARM_HOST_IP sudo -S docker service update --image chheller/stator stator-api
