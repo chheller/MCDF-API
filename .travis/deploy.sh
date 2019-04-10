af62996b127a46c122c7ea447058aa6997e95bb5
Charles <charles@supplypike.com>
2019-04-09 22:52:08 -0500

    pop stash

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,10 +2,10 @@
 set -e
 set -u
 HOST_IP=$1
-openssl aes-256-cbc -K $encrypted_71a8e2a8b8b0_key -iv $encrypted_71a8e2a8b8b0_iv -in .travis/key.enc -out .travis/key -d
+openssl aes-256-cbc -K $key -iv $key_iv,l -in .travis/key.enc -out .travis/key -d
 eval "$(ssh-agent -s)"
 ssh-keyscan -H $HOST_IP >> $HOME/.ssh/known_hosts
 chmod 600 .travis/key
 ssh-add .travis/key
 
-echo $DOCKER_SWARM_USER_PASSWORD | ssh swarm@$HOST_IP sudo -S docker service update --image chheller/stator stator-api
+echo $DOCKER_SWARM_USER_PASSWORD | ssh $SSH_USER@$HOST_IP sudo -S docker service update --image chheller/mcdf-api mcdf-api
